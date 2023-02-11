b50a3c2f02f03909f08005e7046b12e2df2a32c4
Slava Nazimko <vnazimko@gmail.com>
2023-02-11 15:20:37 +0300

    feat: added class Router

--- Museum/AppMain/Router.swift
+++ Museum/AppMain/Router.swift
@@ -5,4 +5,30 @@
 //  Created by Vyacheslav on 11.02.2023.
 //
 
-import Foundation
+import UIKit
+
+protocol RouterProtocol {
+    var moduleBuilder: ModuleBuilderProtocol { get set }
+    var navigationController: UINavigationController  { get set }
+    func showLogInViewController()
+    func showMainViewController()
+}
+
+class Router: RouterProtocol {
+    var moduleBuilder: ModuleBuilderProtocol
+    var navigationController: UINavigationController
+    
+    init(moduleBuilder: ModuleBuilderProtocol, navigationController: UINavigationController) {
+        self.moduleBuilder = moduleBuilder
+        self.navigationController = navigationController
+    }
+    
+    func showLogInViewController() {
+        
+    }
+    
+    func showMainViewController() {
+        
+    }
+
+}
